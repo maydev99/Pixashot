b110390434fc58742226ecc22dcd4cc6bc4ec5a1
michael <maydev99@gmail.com>
2021-02-04 18:43:45 -0500

    Added Fragment and Bottom Nav

---
+++ app/src/main/java/com/bombadu/pixashot/MainActivity2.kt
@@ -0,0 +1,28 @@
+package com.bombadu.pixashot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity2 : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setupNavigation()
+
+    }
+
+    private fun setupNavigation() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
